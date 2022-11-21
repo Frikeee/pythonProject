abaf5773d790d0941b1391c0f4c316ae53ca9eea
UserZI <mergel03@gmail.com>
2022-11-21 16:15:42 +0700

    All File

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from threading import *
 
 
 class Probability():
-    """Рассчёт доврительной вероятности"""
+    """Расчёт доверительной вероятности"""
     def __init__(self):
         self.file = ''
         self.y = []
@@ -52,7 +52,7 @@ class Probability():
         ax.set_ylabel("Герц")
         plt.show()
 
-    def is_valid(self,newval):
+    def is_valid(self, newval):
         """Проверка написания доверительной вероятности"""
         return re.match("^\d{0,1}\.{0,1}\d{0,2}$", newval) is not None
 
@@ -60,47 +60,51 @@ class Probability():
         """Постройка временной диаграммы файла"""
         fig, ax = plt.subplots(nrows=1, sharex=True, sharey=True)
         librosa.display.waveshow(self.y, sr=self.sr, ax=ax)
-        ax.set(title='Спектр аудиофайла')
+        ax.set(title='Временной спектр аудиофайла')
         ax.set_xlabel("Время")
         ax.set_ylabel("Амплитуда")
         ax.label_outer()
         plt.show()
 
+    def calcdB(self):
+        Db = librosa.amplitude_to_db(self.S, ref=np.max, top_db=None)
+        clearDB = []
+        summar = 0
+        for i in range(len(Db[1])):
+            for z in range(len(Db)):
+                summar += Db[z][i] / 1025
+            clearDB.append(round(abs(summar)))
+            summar = 0
+        D = clearDB
+        D.sort()
+        DecibelMAX = np.max(D)
+        self.DecibelMIN = np.min(D)
+        renges = 0
+        Gistoram = []
+        iterat = 0
+        for i in range(DecibelMAX - self.DecibelMIN + 1):
+            self.DecibelArr.append(self.DecibelMIN + i)
+        while renges <= DecibelMAX - self.DecibelMIN:
+            for i in D:
+                if self.DecibelMIN + renges == i:
+                    iterat += 1
+            Gistoram.append(iterat)
+            iterat = 0
+            renges += 1
+        print(Gistoram)
+        prevValue = 0
+        for i in Gistoram:
+            prevValue = prevValue + i / sum(Gistoram)
+            self.finalyMass.append(float("{0:.5f}".format(prevValue)))
+        print(self.finalyMass)
+        self.fileLoad = True
+
+
     def probabFunc(self):
         """Высчитывание доверительной информации"""
         userProb = (float)(userProbTF.get())
         if self.fileLoad == False:
-            Db = librosa.amplitude_to_db(self.S, ref=np.max, top_db=None)
-            clearDB = []
-            summar = 0
-            for i in range(len(Db[1])):
-                for z in range(len(Db)):
-                    summar += Db[z][i] / 1025
-                clearDB.append(round(abs(summar)))
-                summar = 0
-            D = clearDB
-            D.sort()
-            DecibelMAX = np.max(D)
-            self.DecibelMIN = np.min(D)
-            renges = 0
-            Gistoram = []
-            iterat = 0
-            for i in range(DecibelMAX - self.DecibelMIN + 1):
-                self.DecibelArr.append(self.DecibelMIN + i)
-            while renges <= DecibelMAX - self.DecibelMIN:
-                for i in D:
-                    if self.DecibelMIN + renges == i:
-                        iterat += 1
-                Gistoram.append(iterat)
-                iterat = 0
-                renges += 1
-            print(Gistoram)
-            prevValue = 0
-            for i in Gistoram:
-                prevValue = prevValue + i / sum(Gistoram)
-                self.finalyMass.append(float("{0:.5f}".format(prevValue)))
-            print(self.finalyMass)
-            self.fileLoad = True
+            self.calcdB()
         for index in range(len(self.finalyMass)):
             if self.finalyMass[index] > userProb:
                 textProb["text"] = "Доверительная громкость: " + str(index + self.DecibelMIN) + " dB"
@@ -148,9 +152,6 @@ class Probability():
 if __name__ == '__main__':
     mainWindow = Tk()
     prob = Probability()
-
-
-
     mainWindow['bg'] = '#AFEEEE'
     mainWindow.title('Trust Sound')
     mainWindow.geometry('300x300')
@@ -168,7 +169,7 @@ if __name__ == '__main__':
     userProbTF.pack(anchor="w", padx=5)
     textProb = Label(frame, bg='#ADD8E6')
     textProb.pack(anchor="w", padx=5)
-    btn = Button(frame, text='Спектр аудиофайла', command=prob.audioSpec)
+    btn = Button(frame, text='Временной спектр аудиофайла', command=prob.audioSpec)
     btn.pack(anchor="w", padx=5, pady=15)
     btn = Button(frame, text='Cпектрограмма мощности', command=prob.openPowerSpec)
     btn.pack(anchor="w", padx=5)
